155637cc67346b28fadd21ed316929f1685df0e6
AnitaBhadel <bhadelanita@gmail.com>
2017-05-01 08:57:39 -0400

    Add files via upload
    Added pages and tests Folders

---
+++ DemoProject/DemoProject/DemoProject/DemoProject/DemoQA_PO/registration_page.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject.DemoQA_PO
+{
+    class registration_page
+    {
+        private Iwebdriver driver;
+    }
+}
